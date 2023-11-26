1b19ea6cd31091460c9f36d3228397b1b5d96ff5
gabrielajazc <gabrielajazc@gmail.com>
2023-11-27 01:59:31 +0700

    gaby 27/11/2023 advanced search

--- pages/advancedSearch/index.js
+++ pages/advancedSearch/index.js
@@ -12,18 +12,29 @@ import {
   ListItemText,
   Pagination,
   ButtonGroup,
+  Autocomplete,
 } from '@mui/material';
 import SearchIcon from '@mui/icons-material/Search';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 import dayjs, { Dayjs } from 'dayjs';
+import { useRouter } from 'next/router';
 
 const IndexPage = ({ onSelectLetter }) => {
   const [selectedLetter, setSelectedLetter] = useState(null);
   const [startDate, setStartDate] = React.useState(null);
   const [endDate, setEndDate] = React.useState(null);
   const today = dayjs();
+  const optionsAV = ['Network', 'Adjacent Network', 'Local', 'Physical'];
+  const optionsAC = ['High', 'Low'];
+  const optionsScope = ['Changed', 'Unchanged'];
+  const optionsUI = ['Required', 'None'];
+  const optionsUR = ['High', 'Low', 'None'];
+  const optionsConfidentiality = ['High', 'Low', 'None'];
+  const optionsIntegrity = ['High', 'Low', 'None'];
+  const optionsAvailability = ['High', 'Low', 'None'];
+  const router = useRouter();
 
   const handleStartDateChange = (newStartValue) => {
     setStartDate(newStartValue);
@@ -64,14 +75,48 @@ const IndexPage = ({ onSelectLetter }) => {
       <Divider sx={{ my: 2 }} />
 
       <Grid container justifyContent="space-between">
-        <List sx={{ width: '100%', bgcolor: 'background.paper' , padding:'1px', border:'1px solid red'}}>
+        <List sx={{ width: '100%', bgcolor: 'background.paper' }}>
         <ListItem>
+          <Stack direction={{ xs: 'column', sm: 'row' }} width={'100%'} spacing={2} >
           
-          <Grid container direction={'column'} spacing={2} justifyContent="space-between">
+          <Grid item direction={'column'} spacing={2} width={'100%'}>
           <ListItemText primary="Published Dates" />
 
 
-          <Grid container spacing={2} justifyContent="space-between">
+          <Grid container spacing={2} >
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+              <Grid item xs={12} sm={6}>
+                <DatePicker
+                  label="Start Date"
+                  value={startDate}
+                  onChange={handleStartDateChange}
+                  maxDate={today}
+                  renderInput={(params) => (
+                    <TextField {...params} size="small" fullWidth />
+                  )}
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <DatePicker
+                  label="End Date"
+                  value={endDate}
+                  onChange={handleEndDateChange}
+                  maxDate={today}
+                  renderInput={(params) => (
+                    <TextField {...params} size="small" fullWidth />
+                  )}
+                  sx={{ width: '100%' }}
+                />
+              </Grid>
+            </LocalizationProvider>
+          </Grid>
+           </Grid>
+
+          <Grid item direction={'column'} spacing={2} width={'100%'}>
+          <ListItemText primary="Updated Dates" />
+
+
+          <Grid container spacing={2}>
             <LocalizationProvider dateAdapter={AdapterDayjs}>
               <Grid item xs={12} sm={6}>
                 <DatePicker
@@ -99,45 +144,222 @@ const IndexPage = ({ onSelectLetter }) => {
             </LocalizationProvider>
           </Grid>
           </Grid>
-
-          <Grid container direction={'column'} spacing={2} justifyContent="space-between">
-          <ListItemText primary="Published Dates" />
-
-
-          <Grid container spacing={2} justifyContent="space-between">
-            <LocalizationProvider dateAdapter={AdapterDayjs}>
-              <Grid item xs={12} sm={6}>
-                <DatePicker
-                  label="Start Date"
-                  value={startDate}
-                  onChange={handleStartDateChange}
-                  maxDate={today}
-                  renderInput={(params) => (
-                    <TextField {...params} size="small" fullWidth />
-                  )}
-                />
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <DatePicker
-                  label="End Date"
-                  value={endDate}
-                  onChange={handleEndDateChange}
-                  maxDate={today}
-                  renderInput={(params) => (
-                    <TextField {...params} size="small" fullWidth />
-                  )}
-                  sx={{ width: '100%' }}
-                />
-              </Grid>
-            </LocalizationProvider>
-          </Grid>
-          </Grid>
+          </Stack>
         </ListItem>
+
           <ListItem>
-            <ListItemText primary="CWE ID" />
+            <Stack direction={{ xs: 'column', sm: 'row' }} width={'100%'} spacing={2} >
+              <Grid item direction={'column'} spacing={2} width={'100%'}>
+                <ListItemText primary="CWE ID" />
+                <TextField
+                  placeholder="Insert CWE ID"
+                      required
+                      id="outlined-required"
+                      fullWidth
+                      size="small"
+                    renderInput={(params) => (
+                      <TextField {...params} size="small" />
+                    )}
+                  />
+              </Grid>
+              <Grid item direction={'column'} spacing={2} width={'100%'}>
+                <ListItemText primary="CWE Name" />
+                <TextField
+                  placeholder="Insert CWE Name"
+                      required
+                      id="outlined-required"
+                      fullWidth
+                      size="small"
+                    renderInput={(params) => (
+                      <TextField {...params} size="small" />
+                    )}
+                  />
+              </Grid>
+            </Stack>
           </ListItem>
 
-          <ListItem>{/* Add your content here */}</ListItem>
+          <ListItem>
+          <Stack direction={{ xs: 'column', sm: 'row' }} width={'100%'} spacing={2} >
+          <Grid item direction={'column'} spacing={2} width={'100%'}>
+          <ListItemText primary="Vendor" />
+          <TextField
+            placeholder="Insert Vendor"
+                required
+                id="outlined-required"
+                fullWidth
+                size="small"
+              renderInput={(params) => (
+                <TextField {...params} size="small" />
+              )}
+            />
+          </Grid>
+          <Grid item direction={'column'} spacing={2} width={'100%'}>
+          <ListItemText primary="Product" />
+          <TextField
+            placeholder="Insert Product"
+                required
+                id="outlined-required"
+                fullWidth
+                size="small"
+              renderInput={(params) => (
+                <TextField {...params} size="small" />
+              )}
+            />
+          </Grid>
+          <Grid item direction={'column'} spacing={2} width={'100%'}>
+          <ListItemText primary="Platform" />
+          <TextField
+            placeholder="Insert Platform"
+                required
+                id="outlined-required"
+                fullWidth
+                size="small"
+              renderInput={(params) => (
+                <TextField {...params} size="small" />
+              )}
+            />
+          </Grid>
+          
+          </Stack>
+          </ListItem>
+          
+          <ListItem>
+            <Stack direction={{ xs: 'column', sm: 'row' }} width={'100%'} spacing={2} >
+              <Grid item direction={'column'} spacing={2} width={'100%'}>
+                <ListItemText primary="Attack Vector" />
+                <Autocomplete
+                  options={optionsAV}
+                  sx={{ backgroundColor: "white", width: "100%",}}
+                  size="small"
+                  fullWidth
+                  renderInput={(params) => (
+                    <TextField {...params} size="small"  placeholder="Low"    />
+                  )}
+                />
+              </Grid>
+              <Grid item direction={'column'} spacing={2} width={'100%'}>
+                <ListItemText primary="Attack Complexity" />
+                <Autocomplete
+                  options={optionsAC}
+                  sx={{ backgroundColor: "white", width: "100%",}}
+                  size="small"
+                  fullWidth
+                  renderInput={(params) => (
+                    <TextField {...params} size="small"  placeholder="Low"    />
+                  )}
+                />
+              </Grid>
+              <Grid item direction={'column'} spacing={2} width={'100%'}>
+                <ListItemText primary="Scope" />
+                <Autocomplete
+                  options={optionsScope}
+                  sx={{ backgroundColor: "white", width: "100%",}}
+                  size="small"
+                  fullWidth
+                  renderInput={(params) => (
+                    <TextField {...params} size="small"  placeholder="Low"    />
+                  )}
+                />
+              </Grid>
+              <Grid item direction={'column'} spacing={2} width={'100%'}>
+                <ListItemText primary="User Interaction" />
+                <Autocomplete
+                  options={optionsUI}
+                  sx={{ backgroundColor: "white", width: "100%",}}
+                  size="small"
+                  fullWidth
+                  renderInput={(params) => (
+                    <TextField {...params} size="small"  placeholder="Low"    />
+                  )}
+                />
+              </Grid>
+            </Stack>
+          </ListItem>
+
+          <ListItem>
+            <Stack direction={{ xs: 'column', sm: 'row' }} width={'100%'} spacing={2} >
+              <Grid item direction={'column'} spacing={2} width={'100%'}>
+                <ListItemText primary="Privileges Required" />
+                <Autocomplete
+                  options={optionsUR}
+                  sx={{ backgroundColor: "white", width: "100%",}}
+                  size="small"
+                  fullWidth
+                 
+                  renderInput={(params) => (
+                    <TextField {...params} size="small"  placeholder="Low"    />
+                  )}
+                />
+              </Grid>
+              <Grid item direction={'column'} spacing={2} width={'100%'}>
+                <ListItemText primary="Confidentiality" />
+                <Autocomplete
+                  options={optionsConfidentiality}
+                  sx={{ backgroundColor: "white", width: "100%",}}
+                  size="small"
+                  fullWidth
+                  
+                  renderInput={(params) => (
+                    <TextField {...params} size="small"  placeholder="Low"    />
+                  )}
+                />
+              </Grid>
+              <Grid item direction={'column'} spacing={2} width={'100%'}>
+                <ListItemText primary="Integrity" />
+                <Autocomplete
+           
+                  options={optionsIntegrity}
+                  sx={{ backgroundColor: "white", width: "100%",}}
+                  size="small"
+                  fullWidth
+                 
+                  renderInput={(params) => (
+                    <TextField {...params} size="small"  placeholder="Low"    />
+                  )}
+                />
+              </Grid>
+              <Grid item direction={'column'} spacing={2} width={'100%'}>
+                <ListItemText primary="Availability" />
+                <Autocomplete
+           
+                  options={optionsAvailability}
+                  sx={{ backgroundColor: "white", width: "100%",}}
+                  size="small"
+                  fullWidth
+                 
+                  renderInput={(params) => (
+                    <TextField {...params} size="small"  placeholder="Low"    />
+                  )}
+                />
+              </Grid>
+            </Stack>
+          </ListItem>
+          
+          <ListItem>
+            <Grid
+                container
+                item
+                sx={{
+                  py: '10px',
+                }}
+                direction={'row-reverse'}  // Change 'flex-end' to 'row-reverse'
+              >
+                <Grid item >
+                  <Button
+                    variant="contained"
+                    size="large"  // Change 'Large' to 'large'
+                    sx={{backgroundColor:'#8EB4F4'}}
+                    onClick={() => {
+                      router.push(`/cvelist`);
+                    }}
+                    startIcon={ <SearchIcon />}
+                  >
+                  
+                    Search
+                  </Button>
+                </Grid>
+              </Grid>
+            </ListItem>
         </List>
       </Grid>
 
